b526af436f96d584a2b2a44f75fbc251f249299f
Jair Cárdenas <jose.cardenas@avicanna.com>
2023-06-29 11:29:58 -0500

    segundo pusheo de prueba

--- M03 JavaScript Fundamentos/Ejercicio 02.js
+++ M03 JavaScript Fundamentos/Ejercicio 02.js
@@ -13,40 +13,40 @@ function suma(x, y) {
    // Retorna el resultado de su suma.
    // Tu código:
    return(x+y);
-   x=3;
-   y=4;
+   let x=3;
+   let y=4;
 }
 
 function resta(x, y) {
    // Retorna el resultado de la resta.
    // Tu código:
    return(x-y);
-   x=4;
-   y=3;
+   let x=4;
+   let y=3;
 }
 
 function divide(x, y) {
    // Retorna el resultado de su división.
    // Tu código:
    return(x/y);
-   x=4;
-   y=2;
+   let x=4;
+   let y=2;
 }
 
 function multiplica(x, y) {
    // Retorna el resultado de su multiplicación.
    // Tu código:
    return(x*y);
-   x=3;
-   y=2;
+   let x=3;
+   let y=2;
 }
 
 function obtenerResto(x, y) {
    // Obten el resto de la división de "x" entre "y".
    // Tu código:
    return(x%y);
-   x=10;
-   y=3;
+   let x=10;
+   let y=3;
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
